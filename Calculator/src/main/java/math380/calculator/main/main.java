f7279c0630a243334bcd888299663be1aa5621af
alfred <alfred@alfred.local>
2015-04-10 14:11:21 -0400

    updated project

---
+++ Calculator/src/main/java/math380/calculator/main/main.java
@@ -0,0 +1,13 @@
+/*
+ * Class: MATH 380-001
+ * Project: Calculator
+ */
+package math380.calculator.main;
+
+/**
+ *
+ * @author Jonathan Butler <https://github.com/jackal390iv>
+ */
+public class main {
+    
+}
